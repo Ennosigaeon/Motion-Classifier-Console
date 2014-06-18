e52a30661df9186da910ba6056280c3f9b01857d
Marc <mazoeller@gmail.com>
2014-06-18 13:47:18 +0200

    added global namespace motion_classifier

--- Main.cpp
+++ Main.cpp
@@ -4,7 +4,6 @@
 #include "h/EMGProvider.h"
 #include "h/EMGFileProvider.h"
 #include "h/EMGOTProvider.h"
-#include "h/Exception.h"
 #include "h/MultiClassSVM.h"
 #include "h/Trainer.h"
 #include "h/Classifier.h"
@@ -18,18 +17,12 @@
 int main(int argc, char *argv[]) {
 	//loads all configurations and inits the logging system
 	try {
-		AppConfig::load(argc, argv);
-		AppConfig::getInstance()->initLogging();
+		motion_classifier::AppConfig::load(argc, argv);
+		motion_classifier::AppConfig::getInstance()->initLogging();
 	}
-	catch (int ex) {
-		if (ex == Exception::NO_CONFIGURATIONS_DEFINED)
-			std::cerr << "Unable to find path to configuration file" << std::endl
-			<< "Online Classificator usage: " << AppConfig::CONFIG_ARGUMENT << " <path_to_config>" << std::endl;
-		if (ex == Exception::UNABLE_TO_OPEN_FILE)
-			std::cerr << "Unable to read configurations from configuration file. Not possible to open the file." << std::endl;
-		if (ex == Exception::UNABLE_TO_PARSE_CONFIGURATION)
-			std::cerr << "Unable to parse configurations. Please check all configurations." << std::endl;
-		return EXIT_FAILURE;
+	catch (std::invalid_argument& ex) {
+		std::cerr << "Exception while loading configuration: " << ex.what() << std::endl;
+		std::cerr << "Motion-Classifier usage: " << motion_classifier::AppConfig::CONFIG_ARGUMENT << " <path_to_config>" << std::endl;
 	}
 
 	std::string path = "c:\\Users\\Marc\\Dropbox\\Informatik\\Studium\\6. Semester\\Bachelor Thesis\\MARC\\data\\data8_AN-format.txt";
@@ -39,21 +32,21 @@ int main(int argc, char *argv[]) {
 	//of the program is reached.
 	{
 		//EMGOTProvider emgProvider{};
-		EMGFileProvider emgProvider{ path };
-		MultiClassSVM svm;
+		motion_classifier::EMGFileProvider emgProvider{ path };
+		motion_classifier::MultiClassSVM svm;
 
-		Trainer trainer(&emgProvider, &svm);
+		motion_classifier::Trainer trainer(&emgProvider, &svm);
 		//trainer.train();
 
-		Classifier classifier(&emgProvider, &svm);
-		classifier.send(Signal::START);
+		motion_classifier::Classifier classifier(&emgProvider, &svm);
+		classifier.send(motion_classifier::Signal::START);
 		Sleep(30000);
-		classifier.send(Signal::SHUTDOWN);
+		classifier.send(motion_classifier::Signal::SHUTDOWN);
 	}
 
 	//closes all open resources, releases heap memory, ...
 	Plotter::release();
-	AppConfig::release();
+	motion_classifier::AppConfig::release();
 
 	//Only used to keep the terminal still visible, when the program is finished
 	std::cout << "Please press Enter to close program... ";
