431be9c4885ca81e4d49c057bbaccad9ef40dccc
Marc Zoeller <mazoeller@gmail.com>
2014-07-08 14:10:30 +0200

    Added Logger

--- Main.cpp
+++ Main.cpp
@@ -1,13 +1,15 @@
 
 #include <iostream>
 #include "h/AppConfig.h"
-#include "h/EMGProvider.h"
 #include "h/EMGFileProvider.h"
-#include "h/EMGOTProvider.h"
+#include "h/Logger.h"
 #include "h/MultiClassSVM.h"
+#include "h/Properties.h"
+#include "h/SVMClassifier.h"
+#include "h/Sample.h"
 #include "h/Trainer.h"
-#include "h/Classifier.h"
-#include "h/Plotter.h"
+
+void testLogger(std::string message);
 
 /**
 * It is necessary to pass an argument with the path to the configuration file.
@@ -15,10 +17,11 @@
 * with a whitespace before the path.
 */
 int main(int argc, char *argv[]) {
+
 	//loads all configurations and inits the logging system
 	try {
 		motion_classifier::AppConfig::load(argc, argv);
-		motion_classifier::AppConfig::getInstance()->initLogging();
+		Logger::getInstance()->addLogFunction(&testLogger);
 	}
 	catch (std::invalid_argument& ex) {
 		std::cerr << "Exception while loading configuration: " << ex.what() << std::endl;
@@ -31,25 +34,33 @@ int main(int argc, char *argv[]) {
 	//Therefor I added this block, so that both are destroyed before the end
 	//of the program is reached.
 	{
+		//configuration for SVMClassifier
+		Properties prop("svm_config.txt");
+
 		//EMGOTProvider emgProvider{};
 		motion_classifier::EMGFileProvider emgProvider{ path };
-		motion_classifier::MultiClassSVM svm;
+		motion_classifier::EMGProviderImpl *p = &emgProvider;
+		motion_classifier::MultiClassSVM svm(&prop);
+		motion_classifier::SVMClassifier classifier{ &emgProvider, &svm, &prop };
 
-		motion_classifier::Trainer trainer(&emgProvider, &svm);
-		//trainer.train();
+		//motion_classifier::Trainer trainer{};
+		//auto trainingsData = trainer.train(&emgProvider);
+		//classifier.train(trainingsData);
 
-		motion_classifier::Classifier classifier(&emgProvider, &svm);
 		classifier.send(motion_classifier::Signal::START);
-		Sleep(30000);
+		std::this_thread::sleep_for(std::chrono::milliseconds(30000));
 		classifier.send(motion_classifier::Signal::SHUTDOWN);
 	}
 
 	//closes all open resources, releases heap memory, ...
-	Plotter::release();
 	motion_classifier::AppConfig::release();
 
 	//Only used to keep the terminal still visible, when the program is finished
 	std::cout << "Please press Enter to close program... ";
 	std::cin.get();
 	return EXIT_SUCCESS;
+}
+
+void testLogger(std::string message) {
+	std::cout << message << std::endl;
 }
